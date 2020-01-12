d22ead219e9d1a8265b31f1f2dddebca9ec5d689
kvoncode <kvoncode@gmail.com>
2020-01-12 11:16:58 +0900

    add: node-fetch, elon graphql request

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,10 @@ import styled from "styled-components";
 import { useState } from "react";
 import { ResponseView } from "../components/ResponseView";
 
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
+import fetch from "node-fetch";
+
 const StyledMain = styled.div`
   min-height: calc(100vh - 100px);
   min-width: 100vw;
@@ -13,6 +17,25 @@ const StyledMain = styled.div`
   align-items: center;
   flex-direction: column;
 `;
+
+const url = "https://api.spacex.land/graphql/";
+
+const client = new ApolloClient({
+  uri: url,
+  fetch: fetch
+});
+
+client
+  .query({
+    query: gql`
+      {
+        company {
+          ceo
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
 
 const Main = () => {
   const [token, setToken] = useState("Insert token");
